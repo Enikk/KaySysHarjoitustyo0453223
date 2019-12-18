1f1a9a32fbd6124485c765ce14f2649156dfac97
Enikk <58461825+Enikk@users.noreply.github.com>
2019-12-19 01:57:46 +0200

    Update my-cat.c

--- Htyo_1/my-cat.c
+++ Htyo_1/my-cat.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* http://cprogrammingwithlinux.blogspot.com/2011/10/c-program-to-simulate-linux-cat-command.html */
+/*lähde: http://cprogrammingwithlinux.blogspot.com/2011/10/c-program-to-simulate-linux-cat-command.html */
 
 int main(int argc, char *argv[]){
 	
-	FILE *fp;
+	FILE *file;
 	char fname[350], ch;	
 	int i;
 
-	if(argc < 2){
-		printf("There is a syntax problem.\n");
+	if(argc < 2){ //tarkistaa syntaxin
+		printf("There is a syntax problem. 'Cat File File2 etc'\n");
 		return 0;
 	}
 	
-	for(i=1; i<argc; i++){
+	for(i=1; i<argc; i++){	//käy läpi annetun tiedoston
 		strncpy(fname, argv[i], 350);
-		fp=fopen(fname, "r");
-		if(fp == NULL){
-			printf("No file found\n");
+		file=fopen(fname, "r");
+		if(file == NULL){
+			printf("No file found\n");	
 			return 0;
 		}
-		while((ch=fgetc(fp)) != EOF){
-			putchar(ch);
+		while((ch=fgetc(file)) != EOF){
+			putchar(ch);	//kirjoittaa tiedoston sisällön
 		}
-		fclose(fp);
+		fclose(file);
 	}
 	return 0;
 }
