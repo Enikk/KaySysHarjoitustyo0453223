0f6b94ed4af9f0ba722d2280dae29dd6292048b7
Enikk <58461825+Enikk@users.noreply.github.com>
2019-12-19 02:00:05 +0200

    Update my-unzip.c

--- Htyo_1/my-unzip.c
+++ Htyo_1/my-unzip.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]){
 	while(!feof(fin)){	
 		fread(&count, sizeof(int), 1, fin);
 		fread(&c, sizeof(char), 1, fin);	
-		for(i = 0; i <= count; i++){
+		for(i = 0; i < count; i++){
 			fwrite(&c, sizeof(char), 1, fout);	
 		}		
 	}
