eba32e38a71b44020e283b861b6cae0f03613479
Enikk <58461825+Enikk@users.noreply.github.com>
2019-12-19 02:06:11 +0200

    Update my-zip.c

--- Htyo_1/my-zip.c
+++ Htyo_1/my-zip.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-/*c program to conver text file to binary see-programming*/
+/*
+Tekijä Eetu Nikkilä 0453223
+Lähde: c program to conver text file to binary see-programming lähdelistalta
+
+*/
 int main(int argc, char *argv[]){
 	
 	FILE *fin, *fout;
 	int count;
 	char c1, c2;
-	fin=fopen(argv[1], "r");
+	fin=fopen(argv[1], "r");			//tiedostot
 	fout=fopen(argv[2], "wb");
 	fread(&c1, sizeof(char), 1, fin);	
 	
 	while(!feof(fin)){
 		count=0;		
 		do{			
-			fread(&c2, sizeof(char), 1, fin);
+			fread(&c2, sizeof(char), 1, fin);	//rle pyöritys
 			count++;
 		}while(c1 == c2 && c2 != EOF && count < 255);
 	
-		fwrite(&count, sizeof(int), 1, fout);		
+		fwrite(&count, sizeof(int), 1, fout);		//tiedostoon kirjoitus
 		fwrite(&c1, sizeof(char), 1, fout);
 		c1 = c2; 
 	}	
