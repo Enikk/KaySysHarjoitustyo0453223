a619fa88e050d1a281d1b511b72a14a1e6126f5b
Enikk <58461825+Enikk@users.noreply.github.com>
2019-12-19 02:08:04 +0200

    Update my-unzip.c

--- Htyo_1/my-unzip.c
+++ Htyo_1/my-unzip.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-/*c program to conver text file to binary see-programming*/
+/*
+Tekijä Eetu nikkilä 0453223
+lähdelistalta lähteenä: c program to conver text file to binary see-programming
+*/
 int main(int argc, char *argv[]){
 	FILE *fin, *fout;
 
-	fin=fopen(argv[1], "rb");
+	fin=fopen(argv[1], "rb");	//tiedostot
 	fout=fopen(argv[2], "w");
 
 	char c;
 	int count, i;
 
 	while(!feof(fin)){	
-		fread(&count, sizeof(int), 1, fin);
+		fread(&count, sizeof(int), 1, fin);	//koodin rle purkaminen
 		fread(&c, sizeof(char), 1, fin);	
 		for(i = 0; i < count; i++){
-			fwrite(&c, sizeof(char), 1, fout);	
+			fwrite(&c, sizeof(char), 1, fout);	//tiedostoon kirjoitus
 		}		
 	}
 		
