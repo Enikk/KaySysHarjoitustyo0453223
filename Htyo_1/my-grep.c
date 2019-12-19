7b514daea38366aa633305b1b577d3b539b75ba8
Enikk <58461825+Enikk@users.noreply.github.com>
2019-12-19 02:04:06 +0200

    Update my-grep.c

--- Htyo_1/my-grep.c
+++ Htyo_1/my-grep.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-/*http://codetrays.blogspot.com/2015/09/implementation-of-grep-command-in-c.html
-Need to change to getline()
+#define BUFFER_MAX 5120
+/*
+Tekijä: Eetu Nikkilä 0453223
+Lähteenä 
+http://codetrays.blogspot.com/2015/09/implementation-of-grep-command-in-c.html
+
 */
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char fline[350];
+	FILE *file;
+	char fline[BUFFER_MAX];
 	int i;
 	
-	if(argc<3){
+	if(argc<3){		//tarkistaa argumenttien määrä
 		printf("Syntax error\n");
 		return 0;
 	} 
 	
-	for(i=2; i<argc;i++){
-		fp=fopen(argv[i], "r");
-
-		while(fscanf(fp, "%[^\n]\n", fline)!=EOF){
+	for(i=2; i<argc;i++){	//tiedostot for loopissa mikäli enemmän kuin 1
+		file=fopen(argv[i], "r");
+		
+		//hakee rivit tiedostosta
+		while(fscanf(file, "%[^\n]\n", fline)!=EOF){
+			
+			//printtaa rivit joissa löytyy haku
 			if(strstr(fline, argv[1]) != NULL){
 				printf("%s\n", fline);	
 			}
-			else{
-				continue;		
-			}
 
 		}
-		fclose(fp);
+		fclose(file);
 	}
 
 }
